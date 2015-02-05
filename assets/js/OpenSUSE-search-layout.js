16bce885d4619ad609d004f9783b1b7173e1d00c
Cynthia Sanchez <csanchez@suse.com>
2015-02-05 19:08:03 +0100

    layout responsive always in the center

---
+++ assets/js/OpenSUSE-search-layout.js
@@ -0,0 +1,35 @@
+var screenHeight   = 0
+var headerHeight   = 0
+var footerHeight   = 0
+var sectionHeight  = 0
+var remaining      = 0
+
+$(window).on("load", function() {
+  sectionHeight  = $('#search-index section').outerHeight();
+  headerHeight   = 30;//$('#search-index header').outerHeight(); => doesnt work correctly in Firefox. Fix and re-aply.
+  footerHeight   = 90;//$('#search-index footer').outerHeight(); => doesnt work correctly in Firefox. Fix and re-aply.
+  layout_fix();
+
+});
+
+$(window).on("resize", function() {
+
+  layout_fix();
+
+});
+
+function layout_fix () {
+  screenHeight = $(window).outerHeight();
+  remaining    = screenHeight - footerHeight - headerHeight;
+
+  if ( remaining > sectionHeight ) {
+    $('#search-index .fixed-column-form').css({
+      'padding-top': ((remaining - sectionHeight) / 2) + 'px'
+    })
+    $('#search-index .container-fluid').css({
+      height: remaining + headerHeight + 'px'
+    })
+
+  }
+}
+

d72f770a0f1fa1047d77c207ab37f6c46abb61e9
Cynthia Sanchez <csanchez@suse.com>
2015-09-09 13:24:35 +0200

    Fix: dropup of translations open to the right now. input field size increased

--- assets/js/OpenSUSE-search-layout.js
+++ assets/js/OpenSUSE-search-layout.js
@@ -4,13 +4,12 @@ var footerHeight   = 0
 var sectionHeight  = 0
 var remaining      = 0
 
-$(window).on("load", function() {
+function getScreenSize () {
   sectionHeight  = $('#search-index section').outerHeight();
-  headerHeight   = 30;//$('#search-index header').outerHeight(); => doesnt work correctly in Firefox. Fix and re-aply.
+  headerHeight   = 50;//$('#search-index header').outerHeight(); => doesnt work correctly in Firefox. Fix and re-aply.
   footerHeight   = 60;//$('#search-index footer').outerHeight(); => doesnt work correctly in Firefox. Fix and re-aply.
   layout_fix();
-
-});
+};
 
 $(window).on("resize", function() {
 
@@ -20,16 +19,53 @@ $(window).on("resize", function() {
 
 function layout_fix () {
   screenHeight = $(window).outerHeight();
+  searchFormAndDooel = $(".fixed-column-form").outerHeight();
   remaining    = screenHeight - footerHeight - headerHeight;
 
   if ( remaining > sectionHeight ) {
     $('#search-index .fixed-column-form').css({
-      'padding-top': ((remaining - sectionHeight) / 3) + 'px'
-    })
+      'padding-top': ((remaining - searchFormAndDooel) / 4) + 'px'
+    });
     $('#search-index .container-fluid').css({
       height: remaining + headerHeight + 'px'
-    })
+    });
 
   }
 }
 
+//smooth page loader.. because the sudden print on screen bothers me a lot
+$(document).on("ready", function (){
+
+  var loadingMaringTop = $(window).height() / 2
+  $("body").wrapInner( "<div class='opensuse-search-page'></div>");
+  $("body").prepend("<div class='opensuse-loading-page'></div>");
+  $(".opensuse-loading-page").css({
+    position: 'absolute',
+    top: '0px',
+    left: '0px',
+    width: '100%',
+    height: '100%',
+    background: '#fff',
+  });
+  $(".opensuse-loading-page").prepend("<div class='opensuse-loading-text' lang='en'>Loading Page</div>");
+  $(".opensuse-loading-text").css({
+    width: '300px',
+    'margin-left': 'auto',
+    'margin-right': 'auto',
+    'text-align': 'center',
+    'padding-top': loadingMaringTop + 'px'
+  });
+
+  $(".opensuse-search-page").hide();
+
+})
+
+$(window).on("load", function() {
+
+  $(".opensuse-loading-page").fadeOut('600', function() {
+    $(".opensuse-search-page").fadeIn();
+    $(this).remove();
+    getScreenSize();
+  });
+
+});

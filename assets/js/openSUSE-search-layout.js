15bdc7e3eba2f57dfa5f1e9a744868d82ba68bfa
Sarah Julia Kriesch <krieschsa69526@th-nuernberg.de>
2020-06-17 17:59:31 +0200

    add Estonian to languages

--- assets/js/openSUSE-search-layout.js
+++ assets/js/openSUSE-search-layout.js
@@ -83,6 +83,7 @@ window.lang.dynamic('cs', 'assets/js/langpack/cs.json');
 window.lang.dynamic('da', 'assets/js/langpack/da.json');
 window.lang.dynamic('de', 'assets/js/langpack/de.json');
 window.lang.dynamic('el', 'assets/js/langpack/el.json');
+window.lang.dynamic('et', 'assets/js/langpack/et.json');
 window.lang.dynamic('es', 'assets/js/langpack/es.json');
 window.lang.dynamic('fa', 'assets/js/langpack/fa.json');
 window.lang.dynamic('fi', 'assets/js/langpack/fi.json');
